3dbdecc5c9bdbd62ee742f65927fdc227791e266
Kd <86334894+kdlanguido@users.noreply.github.com>
2025-09-12 18:42:47 +0800

    Added header

---
+++ components/Header.tsx
@@ -0,0 +1,167 @@
+"use client"
+
+import * as React from "react"
+import Link from "next/link"
+import { CircleCheckIcon, CircleHelpIcon, CircleIcon } from "lucide-react"
+
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+import Image from "next/image"
+import { Geist_Mono } from "next/font/google"
+
+const components: { title: string; href: string; description: string }[] = [
+    {
+        title: "Alert Dialog",
+        href: "/docs/primitives/alert-dialog",
+        description:
+            "A modal dialog that interrupts the user with important content and expects a response.",
+    },
+    {
+        title: "Hover Card",
+        href: "/docs/primitives/hover-card",
+        description:
+            "For sighted users to preview content available behind a link.",
+    },
+    {
+        title: "Progress",
+        href: "/docs/primitives/progress",
+        description:
+            "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+    },
+    {
+        title: "Scroll-area",
+        href: "/docs/primitives/scroll-area",
+        description: "Visually or semantically separates content.",
+    },
+    {
+        title: "Tabs",
+        href: "/docs/primitives/tabs",
+        description:
+            "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+    },
+    {
+        title: "Tooltip",
+        href: "/docs/primitives/tooltip",
+        description:
+            "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+    },
+]
+
+const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+});
+
+const underlineHover =
+    "relative bg-transparent focus:bg-transparent cursor-pointer " +
+    "after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 " +
+    "after:bg-current after:transition-all after:duration-300 hover:after:w-full"
+
+export function Header() {
+    return (
+        <div className="flex justify-between p-6 px-12 items-center">
+            <div className="flex items-center">
+                <Image className="h-auto" src="/assets/ldlogo.png" alt="" width={50} height={150} />
+                <h1 className={`${geistMono.className} text-[18px] text-white `}>LD Solutions</h1>
+            </div>
+            <NavigationMenu viewport={false}>
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <NavigationMenuTrigger className={underlineHover + "bg-transparent hover:bg-transparent focus:bg-transparent cursor-pointer"}>
+                            Home
+                        </NavigationMenuTrigger>
+                        <NavigationMenuContent>
+                            <ul className="grid gap-2 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr] ">
+                                <li className="row-span-3">
+                                    <NavigationMenuLink asChild>
+                                        <a
+                                            href="/"
+                                            className="from-muted/50 to-muted flex h-full w-full flex-col justify-end rounded-md bg-gradient-to-b p-6 no-underline select-none focus:outline-none focus:shadow-md"
+                                        >
+                                            <div className="mt-4 mb-2 text-lg font-medium">shadcn/ui</div>
+                                            <p className="text-muted-foreground text-sm leading-tight">
+                                                Beautifully designed components built with Tailwind CSS.
+                                            </p>
+                                        </a>
+                                    </NavigationMenuLink>
+                                </li>
+                                <ListItem href="/docs" title="Introduction">
+                                    Re-usable components built using Radix UI and Tailwind CSS.
+                                </ListItem>
+                                <ListItem href="/docs/installation" title="Installation">
+                                    How to install dependencies and structure your app.
+                                </ListItem>
+                                <ListItem href="/docs/primitives/typography" title="Typography">
+                                    Styles for headings, paragraphs, lists...etc
+                                </ListItem>
+                            </ul>
+                        </NavigationMenuContent>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <NavigationMenuTrigger className="bg-transparent hover:bg-transparent focus:bg-transparent cursor-pointer">Services</NavigationMenuTrigger>
+                        <NavigationMenuContent>
+                            <ul className="grid w-[400px] gap-2 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+                                {components.map((component) => (
+                                    <ListItem
+                                        key={component.title}
+                                        title={component.title}
+                                        href={component.href}
+                                    >
+                                        {component.description}
+                                    </ListItem>
+                                ))}
+                            </ul>
+                        </NavigationMenuContent>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink asChild className="font-semibold bg-transparent focus:bg-transparent cursor-pointer">
+                            <Link href="/docs">About Us</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink asChild className="font-semibold bg-transparent focus:bg-transparent cursor-pointer">
+                            <Link href="/docs">Our Projects</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+
+            <NavigationMenu viewport={false}>
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink asChild className="font-semibold bg-transparent focus:bg-transparent cursor-pointer">
+                            <Link href="/contact-us">Contact Us</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+        </div>
+    )
+}
+
+function ListItem({
+    title,
+    children,
+    href,
+    ...props
+}: React.ComponentPropsWithoutRef<"li"> & { href: string }) {
+    return (
+        <li {...props}>
+            <NavigationMenuLink asChild>
+                <Link href={href}>
+                    <div className="text-sm leading-none font-medium">{title}</div>
+                    <p className="text-muted-foreground line-clamp-2 text-sm leading-snug">
+                        {children}
+                    </p>
+                </Link>
+            </NavigationMenuLink>
+        </li>
+    )
+}
