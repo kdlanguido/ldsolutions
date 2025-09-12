c57de028a281c673d49d40fc014d70385dd8d8ec
Kd <86334894+kdlanguido@users.noreply.github.com>
2025-09-12 19:16:30 +0800

    added header items and fav icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,11 @@ import type { Metadata } from "next"
 export const metadata: Metadata = {
   title: "LD Solutions",
   description: "LD Solutions Philippines",
+  icons: {
+    icon: "/favicon.png", // default
+    shortcut: "/favicon.png", // optional
+    apple: "/apple-touch-icon.png", // for iOS
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
--- components/Header.tsx
+++ components/Header.tsx
@@ -18,39 +18,33 @@ import { Geist_Mono } from "next/font/google"
 
 const components: { title: string; href: string; description: string }[] = [
     {
-        title: "Alert Dialog",
-        href: "/docs/primitives/alert-dialog",
+        title: "Strategy and Design",
+        href: "/services/strategy-design",
         description:
             "A modal dialog that interrupts the user with important content and expects a response.",
     },
     {
-        title: "Hover Card",
+        title: "Architecture",
         href: "/docs/primitives/hover-card",
         description:
             "For sighted users to preview content available behind a link.",
     },
     {
-        title: "Progress",
+        title: "Data Management",
         href: "/docs/primitives/progress",
         description:
             "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
     },
     {
-        title: "Scroll-area",
+        title: "Engineering",
         href: "/docs/primitives/scroll-area",
         description: "Visually or semantically separates content.",
     },
     {
-        title: "Tabs",
+        title: "Cloud Modern Systems",
         href: "/docs/primitives/tabs",
         description:
             "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-    },
-    {
-        title: "Tooltip",
-        href: "/docs/primitives/tooltip",
-        description:
-            "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
     },
 ]
 
