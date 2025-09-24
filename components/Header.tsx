dbf86eca684bbe75933f8d435c8876972d472217
King Dranreb Languido <86334894+kdlanguido@users.noreply.github.com>
2025-09-24 16:18:39 +0800

    Deploy to STG (#75)
    * removed LogoLoop.tsx
    
    * update contents
    
    * changed a tag to Link
    
    * push fallback (#46)
    
    * Kd/update/added fallback (#48)
    
    * push fallback
    
    * removed comment
    
    * Kd/update/added fallback (#50)
    
    * push fallback
    
    * push fallback (#47)
    
    * removed comment
    
    * add fallback
    
    * revised fallback ui (#52)
    
    * Kd/update/rewordfallback (#55)
    
    * Deploy to STG (#51)
    
    * push fallback (#46)
    
    * Kd/update/added fallback (#48)
    
    * push fallback
    
    * removed comment
    
    * Kd/update/added fallback (#50)
    
    * push fallback
    
    * push fallback (#47)
    
    * removed comment
    
    * add fallback
    
    * revised fallback ui
    
    * added flex
    
    * Kd/update/rewordfallback (#57)
    
    * revised fallback ui
    
    * added flex
    
    * added space
    
    * header update removed industries (#59)
    
    * Deleted logoLoop.tsx
    
    * adjusted mobile friendly (#61)
    
    * added clause for tablet (#63)
    
    * Kd/update/header for tablet (#65)
    
    * added clause for tablet
    
    * code refactor
    
    * Added homepage (#67)
    
    * push to dev (#74)
    
    ---------
    
    Co-authored-by: Lloyd Languido <79763710+Syrulli@users.noreply.github.com>
    Co-authored-by: jeiobri <90114096+jeiobri@users.noreply.github.com>

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,11 +5,9 @@ import { MenuIcon, PhoneCallIcon } from "lucide-react"
 
 import {
     NavigationMenu,
-    NavigationMenuContent,
     NavigationMenuItem,
     NavigationMenuLink,
     NavigationMenuList,
-    NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu"
 import Image from "next/image"
 import { Geist_Mono } from "next/font/google"
@@ -94,100 +92,34 @@ export function Header() {
     return (
         <>
             {/* Desktop */}
-            <div className="hidden lg:flex items-center justify-between gap-8 fixed top-0 left-0 right-0 z-[100] bg-black/80 backdrop-blur-md p-5 h-[10vh]  ">
+            <div className="hidden lg:flex items-center justify-between gap-8 fixed top-0 left-0 right-0 z-[100] bg-black/80 backdrop-blur-md p-5 h-[10vh]">
                 <Link className="flex items-center" href="/">
                     <Image className="h-auto w-[30] h-[auto] lg:w-[50]" src="/assets/ldlogo.png" alt="" width={50} height={150} />
                     <h1 className={`${geistMono.className} text-[10px] md:text-[18px] text-white `}>LD Solutions</h1>
                 </Link>
                 <NavigationMenu>
-                    <NavigationMenuList >
-                        <NavigationMenuItem>
-                            <NavigationMenuTrigger className={"bg-transparent hover:bg-transparent focus:bg-transparent cursor-pointer"}>
-                                Insights
-                            </NavigationMenuTrigger>
-                            <NavigationMenuContent>
-                                <ul className="grid gap-2 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr] ">
-                                    <li className="row-span-3">
-                                        <NavigationMenuLink asChild>
-                                            <a
-                                                href="/insights/mern-stack"
-                                                className="relative flex h-full w-full flex-col justify-end rounded-md overflow-hidden p-6 no-underline select-none focus:outline-none focus:shadow-md"
-                                            >
-                                                <Image
-                                                    src="/assets/mern.png"
-                                                    alt="MERN Stack"
-                                                    fill
-                                                    className="object-cover absolute inset-0 z-0"
-                                                />
-                                                <div className="absolute inset-0 bg-gradient-to-b from-black/40 to-black/70 z-10"></div>
-                                                <div className="mt-4 mb-2 text-lg font-medium text-white z-20">MERN Stack</div>
-                                                <p className="text-sm leading-tight text-gray-200 z-20">
-                                                    Build dynamic apps with MongoDB, Express, React, and Node.js.
-                                                </p>
-                                            </a>
-                                        </NavigationMenuLink>
-                                    </li>
-
-                                    <ListItem href="/insights/methodologies" title="Methodologies">
-                                        Agile approaches for efficient project delivery.
-                                    </ListItem>
-
-                                    <ListItem href="/insights/nextjs" title="Next JS">
-                                        Framework for fast, scalable web applications.
-                                    </ListItem>
-
-                                    <ListItem href="/insights/modern-software" title="Modern Software Solutions">
-                                        Tools and practices for building future-ready systems.
-                                    </ListItem>
-                                </ul>
-
-
-                            </NavigationMenuContent>
-                        </NavigationMenuItem>
-
-                        <NavigationMenuItem>
-                            <NavigationMenuTrigger className="bg-transparent hover:bg-transparent focus:bg-transparent cursor-pointer">Our Services</NavigationMenuTrigger>
-                            <NavigationMenuContent>
-                                <ul className="grid w-[400px] gap-2 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
-                                    {serviceList.map((service) => (
-                                        <ListItem
-                                            key={service.title}
-                                            title={service.title}
-                                            href={service.href}
-                                        >
-                                            {service.description}
-                                        </ListItem>
-                                    ))}
-                                </ul>
-                            </NavigationMenuContent>
-                        </NavigationMenuItem>
-
-                        <NavigationMenuItem>
-                            <NavigationMenuTrigger className="bg-transparent hover:bg-transparent focus:bg-transparent cursor-pointer">About Us</NavigationMenuTrigger>
-                            <NavigationMenuContent>
-                                <ul className="grid w-[400px] gap-2 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
-                                    {aboutUsList.map((linkData, index) =>
-                                        <ListItem
-                                            key={index}
-                                            title={linkData.title}
-                                            href={linkData.href}
-                                        >
-                                            {linkData.description}
-                                        </ListItem>
-                                    )}
-
-                                </ul>
-                            </NavigationMenuContent>
-                        </NavigationMenuItem>
+                    <NavigationMenuList className="gap-4">
                         <NavigationMenuItem>
                             <NavigationMenuLink asChild className="font-semibold bg-transparent focus:bg-transparent cursor-pointer">
-                                <Link href="/projects">Our Projects</Link>
+                                <Link href="#services">Our Services</Link>
                             </NavigationMenuLink>
                         </NavigationMenuItem>
 
                         <NavigationMenuItem>
                             <NavigationMenuLink asChild className="font-semibold bg-transparent focus:bg-transparent cursor-pointer">
-                                <Link href="/clients">Client Stories</Link>
+                                <Link href="#projects">Our Projects</Link>
+                            </NavigationMenuLink>
+                        </NavigationMenuItem>
+
+                        <NavigationMenuItem>
+                            <NavigationMenuLink asChild className="font-semibold bg-transparent focus:bg-transparent cursor-pointer">
+                                <Link href="#clients">Client Stories</Link>
+                            </NavigationMenuLink>
+                        </NavigationMenuItem>
+
+                        <NavigationMenuItem>
+                            <NavigationMenuLink asChild className="font-semibold bg-transparent focus:bg-transparent cursor-pointer">
+                                <Link href="#projects">About Us</Link>
                             </NavigationMenuLink>
                         </NavigationMenuItem>
 
@@ -242,27 +174,3 @@ export function Header() {
     )
 }
 
-
-
-
-
-
-function ListItem({
-    title,
-    children,
-    href,
-    ...props
-}: React.ComponentPropsWithoutRef<"li"> & { href: string }) {
-    return (
-        <li {...props}>
-            <NavigationMenuLink asChild>
-                <Link href={href}>
-                    <div className="text-sm leading-none font-medium">{title}</div>
-                    <p className="text-muted-foreground line-clamp-2 text-sm leading-snug">
-                        {children}
-                    </p>
-                </Link>
-            </NavigationMenuLink>
-        </li>
-    )
-}
