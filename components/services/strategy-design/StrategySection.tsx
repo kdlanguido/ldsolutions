efe1a07130481649dcfe2d6489c3e724f6adb28f
Lloyd Languido <79763710+Syrulli@users.noreply.github.com>
2025-09-24 20:55:43 +0800

    Lloydm/update/strategy design (#81)
    * Update strategy design layout
    
    * Strategy design page updated components
    
    * Update color scheme for btns and bg image
    
    * update color
    
    * Update color bg

--- components/services/strategy-design/StrategySection.tsx
+++ components/services/strategy-design/StrategySection.tsx
@@ -7,7 +7,7 @@ export default function StrategySection() {
         <section className="text-white mt-7">
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-15 items-center">
                 <div className="mx-auto max-w-7xl px-6 lg:px-15">
-                    <p className="text-sm font-semibold text-[#1e00ff] uppercase">
+                    <p className="text-sm font-semibold text-[#53EAFD] uppercase">
                         Strategy and design
                     </p>
                     <h1 className="mt-4 text-4xl sm:text-5xl font-extrabold tracking-tight">Creating brilliant experiences together</h1>
@@ -28,7 +28,7 @@ export default function StrategySection() {
                             </Button>
 
                             <div
-                                className="border-b-5 border-purple-500 w-full transition-[transform,width] duration-300 ease-out origin-left group-hover:w-[calc(100%+0.25rem)]"
+                                className="border-b-5 border-[#53EAFD] w-full transition-[transform,width] duration-300 ease-out origin-left group-hover:w-[calc(100%+0.25rem)]"
                             />
                         </div>
                     </div>
